19ceecdb1f7e65a1ebcddf5761f6c415e7510d94
Sandro Klippel <sandroklippel@gmail.com>
2024-06-10 16:33:28 -0300

    add :z sufix

--- config.sh
+++ config.sh
@@ -7,6 +7,7 @@ export REGISTRY_ADMIN_PASSWORD="password"
 export REGISTRY_SECRET="e7er2yjacmbqjxsmf6h3rtrh7t6wjhef7bkv6kauv3wng35z"
 export EXTERNAL_URL=""
 export CONTROL_SWITCH='$controlSwitch'
+export DATA_DIR=/data
 
 if test -f ".env"; then
     echo "Loading .env file"
@@ -19,5 +20,5 @@ fi
 rm -rf appsettings.json
 rm -rf initialize.sql
 
-envsubst < appsettings-template.json > appsettings.json
+envsubst < appsettings-template.json > "$DATA_DIR/appsettings.json"
 envsubst < initialize-template.sql > initialize.sql
