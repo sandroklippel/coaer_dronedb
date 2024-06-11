09dd63fb9a664a2c0e592d51ea7921df3d5929d8
Sandro Klippel <sandroklippel@gmail.com>
2024-06-11 16:23:01 -0300

    changed to use sqlite

--- config.sh
+++ config.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-export MYSQL_ROOT_PASSWORD="default-root-password"
-export MYSQL_PASSWORD="default-mysql-password"
+export REGISTRY_ADMIN_USER="admin"
 export REGISTRY_ADMIN_MAIL="test@test.it"
 export REGISTRY_ADMIN_PASSWORD="password"
 export REGISTRY_SECRET="e7er2yjacmbqjxsmf6h3rtrh7t6wjhef7bkv6kauv3wng35z"
-export EXTERNAL_URL=""
-export CONTROL_SWITCH='$controlSwitch'
 export DATA_DIR=/data
+export PORT=5000
 
 if test -f ".env"; then
     echo "Loading .env file"
@@ -18,7 +16,6 @@ else
 fi
 
 rm -rf appsettings.json
-rm -rf initialize.sql
 
 envsubst < appsettings-template.json > "$DATA_DIR/appsettings.json"
-envsubst < initialize-template.sql > initialize.sql
+envsubst < docker-dronedb-template.service /etc/systemd/system/docker-dronedb.service
